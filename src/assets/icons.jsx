ecb4a6c8d59d3a141c22eeb336902476d3ff78fb
Elijah Moye <elijahmoye96@gmail.com>
2025-03-11 17:05:54 -0400

    Deployment issues
    Having deployment issuess

--- src/assets/icons.jsx
+++ src/assets/icons.jsx
@@ -1,8 +1,6 @@
-import binoculars from '/Users/elijahmoye/Desktop/StockApp/src/assets/Icons/binoculars.png'
-import favorites from '/Users/elijahmoye/Desktop/StockApp/src/assets/Icons/favorite.png'
 export const myIcons = {
 
-    binoculars: binoculars, 
-    favorites: favorites
+    binoculars: 'src/assets/Icons/binoculars.png', 
+    favorites: 'src/assets/Icons/favorite.png'
 
 }
