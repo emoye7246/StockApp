3bfc8feaf2bdf441cb80f285e98ff1d4cd0fb513
Elijah Moye <elijahmoye96@gmail.com>
2025-03-11 19:12:42 -0400

    Finished
    Project is completed and deployed got alot of
    editing to do tonight on my later files but
    itll be fun done with this project and proud
    of it

--- src/icons.jsx
+++ src/icons.jsx
@@ -2,6 +2,6 @@ export const myIcons = {
 
     search: '/assets/Fonts/icons/search-interface-symbol.png',
     binoculars: '/assets/Icons/binoculars.png', 
-    favorites: 'public/assets/Icons/favorite.png'
+    favorites: '/assets/Icons/favorite.png'
 
 }
